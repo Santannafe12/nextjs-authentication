d46fdeddd12c164138cc4e9c9120120bd8f27809
santannafe12 <santannafe12@gmail.com>
2024-02-15 00:24:08 -0300

    Add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <></>
+    <div className="h-screen">
+      <h1>Você está logado!</h1>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 }
